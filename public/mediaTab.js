4fd86892e170f10d241bb9ae31302d9707b22ed1
ERAUDBERTHAUDHugo <hugo.eraud-berthaud@isen.yncrea.fr>
2020-06-09 11:27:01 +0200

    affichage des titres news

--- public/mediaTab.js
+++ public/mediaTab.js
@@ -2,10 +2,25 @@
 document.getElementById("media").onclick = function(){
 	var tabNews=[]
 	getReq('/api/news', (result) => {
+		var ongletMedia=document.querySelector(".media.tab-div")
+        console.log(ongletMedia)
+        ongletMedia.innerHTML=""
+        
         console.log(result);
         tabNews=result;
+        var titreArticle=""
         var newZone=addNewzone(media,tabNews.length)// newZone is an array with all the new HTML element "zone"
-       
+
+        
+
+		for (var i= 0; i<tabNews.length;i++){
+			titreArticle=document.createTextNode(tabNews[i].title)
+
+			console.log(titreArticle)
+			var preView=newZone[i].getElementsByClassName("preview")
+			console.log(preView)
+			preView[0].appendChild(titreArticle)
+		}
         
 
     })
