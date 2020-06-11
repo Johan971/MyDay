a7b635e04288f3cade283dec5f1f78fa0488983b
Jogwada <johan.roux@isen.yncrea.fr>
2020-06-12 01:07:31 +0200

    printing tweeter module ok

--- public/mediaTab.js
+++ public/mediaTab.js
@@ -5,6 +5,129 @@ document.getElementById("media").onclick = function(){
     ongletMedia.innerHTML=""
 
 
+        getReq('/api/twitter', (result)=>{
+
+        	
+            var newZone2=addNewzone(media,1)
+            /*
+            let preview = zoneMain[0].children[0]
+        	let fullview = zoneMain[0].children[1]
+    		*/
+
+    		// newZone2.classList.add("twitter")
+
+
+
+    		var preview=newZone2[0].getElementsByClassName("preview")[0]
+    		var fullview=newZone2[0].getElementsByClassName("fullview")[0]
+    		
+
+        	preview.innerHTML = ""
+       		fullview.innerHTML = ""
+
+            
+
+            var titlePv = document.createElement("h1")
+            titlePv.setAttribute("class", "titleTwitterTrend")
+            titlePv.appendChild(document.createTextNode("Tendances"))
+            preview.appendChild(titlePv)
+            
+            var titleFv = document.createElement("h1")
+            titleFv.setAttribute("class", "titleTwitterTrend")
+            titleFv.appendChild(document.createTextNode("Tendances"))
+            fullview.appendChild(titleFv)
+            
+
+            var logotwitter=document.createElement("img")
+    		logotwitter.setAttribute("src", "https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/1200px-Twitter_Bird.svg.png")
+    		logotwitter.setAttribute("class", "logoTwitter")
+    		
+
+    		var canvasPv = document.createElement("canvas")
+    		var canvasFv = document.createElement("canvas")
+
+            var ctx1 = canvasPv.getContext('2d')
+           	var ctx2 = canvasFv.getContext('2d')
+
+           	
+            
+           	
+
+           	logotwitter.onload= ()=>{
+           		ctx1.drawImage(logotwitter, 150, 0, 100, 100)
+           		ctx2.drawImage(logotwitter, 150, 0, 100, 100)
+
+           		// console.log(canvas,ctx)
+           	}
+
+
+    		preview.appendChild(canvasPv)
+
+    		fullview.appendChild(canvasFv)
+    		
+    		
+
+    		for (var previewIterator=0;previewIterator<3;previewIterator++){
+    			separatorPv=document.createElement("hr")
+    			trendNamePv=document.createElement("h2")//titre preView
+    			trendNamePv.appendChild(document.createTextNode(result[previewIterator].name))
+    			preview.appendChild(trendNamePv)
+    			preview.appendChild(separatorPv)
+    			
+    		}
+
+
+    		result.forEach((elm)=>{
+    			
+    			separatorFv=document.createElement("hr")
+    			
+
+    			
+
+    			trendNameFv=document.createElement("h2")//titre preView
+    			trendNameFv.appendChild(document.createTextNode(elm.name))
+    			fullview.appendChild(trendNameFv)
+
+
+    			var explore = document.createElement("p")
+    			
+    			var link=document.createElement("a")
+    			link.setAttribute("href", elm.urlTwitter)
+    			link.appendChild(document.createTextNode("Explorez"))
+    			explore.appendChild(link)
+    			explore.setAttribute("class", "twitterExploreURL")
+    			// explore.setAttribute("href", elm.urlTwitter)
+    			fullview.appendChild(explore)
+
+
+    			if (elm.tweetVolume !=null){
+    				var tweetVolume = elm.tweetVolume
+    				var last24Tweet= document.createElement("p")
+    				last24Tweet.appendChild(document.createTextNode(`${elm.tweetVolume} tweets durant ces dernières 24 heures`))
+    	
+    				fullview.appendChild(last24Tweet)
+    			}
+
+    			fullview.appendChild(separatorFv)
+
+    			
+    			
+    			
+    			
+
+
+    			
+    			
+    		} )
+
+
+   
+
+
+        })
+
+
+
 	getReq('/api/news', (result) => {
 		
         tabNews=result;
@@ -24,12 +147,12 @@ document.getElementById("media").onclick = function(){
 
 
 			var imageNode=document.createElement("img")
-			imageNode.setAttribute("src", tabNews[i].imageUrl);
-			imageNode.setAttribute("class", "imgArcticle");
+			imageNode.setAttribute("src", tabNews[i].imageUrl)
+			imageNode.setAttribute("class", "imgArcticle")
 			preView[0].appendChild(imageNode)
 
+		
 
-			
 			descriptionArtcicle=document.createElement("p")//titre preView
 			descriptionArtcicle.appendChild(document.createTextNode(tabNews[i].description))
 			preView[0].appendChild(descriptionArtcicle)
@@ -37,16 +160,21 @@ document.getElementById("media").onclick = function(){
 			//gestion de la full view (titre + contenu)
 			var fullView=newZone[i].getElementsByClassName("fullview")
 
+
 			var titreFv=document.createElement("h1")
 			titreFv.appendChild(document.createTextNode(tabNews[i].title))//titre fullView
 			fullView[0].appendChild(titreFv)
+
 
 			contenuArticle=document.createElement("p")//titre preView
 			contenuArticle.appendChild(document.createTextNode(tabNews[i].content))
 			fullView[0].appendChild(contenuArticle)
 
+
 			articleLink=document.createElement("a")//titre preView
 			articleLink.setAttribute("href", tabNews[i].articleUrl);
+			
+
 			articleLink.setAttribute("target", "_blank");
 			articleLink.appendChild(document.createTextNode(tabNews[i].articleUrl))
 			fullView[0].appendChild(articleLink)
@@ -55,33 +183,5 @@ document.getElementById("media").onclick = function(){
         
     })
 
-    getReq('/api/twitter', (result)=>{
 
-    	
-        var newZone2=addNewzone(media,1)
-        let preview = zoneMain[0].children[0]
-    	let fullview = zoneMain[0].children[1]
-
-    	preview.innerHTML = ""
-   		fullview.innerHTML = ""
-
-        var logotwitter=document.createElement("img")
-
-        title = document.createElement("h1")
-        title.setAttribute("class", "titlePreview")
-        
-        title.appendChild(document.createTextNode("Tendances"))
-
-
-		logotwitter.setAttribute("src", "https://upload.wikimedia.org/wikipedia/fr/thumb/5/52/Logo-vlille.svg/1200px-Logo-vlille.svg.png")
-		logotwitter.setAttribute("class", "imgPreview")
-		preview.appendChild(logotwitter)
-
-
-		
-
-
-
-
-    })
 }
